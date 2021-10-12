929584ebd0d576fcabe391166158323fd726b244
mat2m10 <matthieu.dehemptinne@gmail.com>
2021-10-12 20:54:41 +0200

    adding tests

--- acronymize/acronymize.rb
+++ acronymize/acronymize.rb
@@ -17,3 +17,11 @@ puts "What is your sentence?"
 sentence = gets.chomp
 
 puts acronymize(sentence)
+
+describe "#acronymize" do
+  it "returns an empty string when passed an empty string" do
+    actual = acronymize("")
+    expected = ""
+    expect(actual).to eq(expected) # passes if `actual == expected`
+  end
+end
