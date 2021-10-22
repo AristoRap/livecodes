b42682947b4f2bd7b1d86b04915caef51703beb8
mat2m10 <matthieu.dehemptinne@gmail.com>
2021-10-22 17:16:54 +0200

    finishing up

--- french_ssn/french_ssn.rb
+++ french_ssn/french_ssn.rb
@@ -1,25 +1,32 @@
 require 'date'
 require 'yaml'
-SSN_PATTERN = /^(?<sex>[12])\s?(?<year>\d{2})\s?(?<month>0[1-9]|1[0-2])\s?(?<zip>\d{2}|2[AB])\s?(\d{3})\s?(\d{3})\s?(?<key>\d{2})$/
+SSN_PATTERN = /^(?<gender>[12])\s?(?<year>\d{2})\s?(?<month>0[1-9]|1[0-2])\s?(?<zip>\d{2}|2[AB])\s?(\d{3})\s?(\d{3})\s?(?<key>\d{2})$/
 
 def french_ssn_info(ssn)
   match_data = ssn.match(SSN_PATTERN)
-  if match_data
-    gender = gender(match_data[:sex])
+  if match_data && valid_key?(ssn, match_data[:key])
+    gender = gender(match_data[:gender])
+    month = month(match_data[:month])
     year = "19#{match_data[:year]}"
-    date = Date::MONTHNAMES[match_data[:month].to_i]
-    departement = YAML.load_file('data/french_departments.yml')
-    departement = departement[match_data[:zip]]
-    p departement
+    department = department(match_data[:zip])
+    return "a #{gender}, born in #{month}, #{year} in #{department}."
   else
-    puts "invalid number"
+    return "The number is invalid"
   end
-  p match_data
-  return "The number is invalid"
 end
 
 def gender(code)
-  return code == "1" ? 'man' : 'woman'
+  return code == "1" ? "man" : "woman"
 end
 
-french_ssn_info("1 84 12 76 451 089 46")
+def month(code)
+  return Date::MONTHNAMES[code.to_i]
+end
+
+def department(code)
+  return YAML.load_file('data/french_departments.yml')[code]
+end
+
+def valid_key?(ssn, key)
+  return (97 - ssn.delete(' ')[0..12].to_i) % 97 == key.to_i
+end
